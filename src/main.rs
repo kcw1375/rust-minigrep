78bb21175fde62c55a39c755416475f990d7ccf0
kcw <dwong1375@gmail.com>
2022-07-16 19:08:24 -0700

    refactor configuration variables into a Config struct

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,25 @@ use std::fs;
 fn main() {
     // read input arguments from stdin
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
     
-    println!("Searching for {query}");
-    println!("In file {filename}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file.");
 
     println!("With text:\n{contents}");
 }
 
+struct Config {
+    query : String,
+    filename : String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].to_string();
+    let filename = args[2].to_string();
 
-    (query, filename)
+    Config { query, filename }
 }
