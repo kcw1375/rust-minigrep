aff872224299ea8ab6f579a3c49d1bce19e572f9
kcw <dwong1375@gmail.com>
2022-07-16 19:10:58 -0700

    turn parse_config fn into a Config constructor for more idiomatic code

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     // read input arguments from stdin
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,9 +20,11 @@ struct Config {
     filename : String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].to_string();
-    let filename = args[2].to_string();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].to_string();
+        let filename = args[2].to_string();
 
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
