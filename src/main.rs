4a58bab3989f10d95dc5de8753e27a7d19c7116e
kcw <dwong1375@gmail.com>
2022-07-16 19:16:15 -0700

    better error handling for argument parser, now returns error messages and gracefully exits instead of panicking

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // read input arguments from stdin
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    // use a closure (anonymous function) to exit the process on error
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -21,10 +26,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments! Must have a query and filename.");
+        }
+
         let query = args[1].to_string();
         let filename = args[2].to_string();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
